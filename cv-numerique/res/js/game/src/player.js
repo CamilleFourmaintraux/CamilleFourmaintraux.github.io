7e7cb2697a6610900b20fdd46e2921e57710048d
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-03-25 17:06:27 +0100

    Update player.js
    UPDATRE MOI CES FICHIERS PLEASE

--- cv-numerique/res/js/game/src/player.js
+++ cv-numerique/res/js/game/src/player.js
@@ -159,6 +159,7 @@ export class Player extends Entity {
 			context.font = '16px Minecraft Regular';
 			context.imageSmoothingEnabled = false;
 			context.fillStyle = 'white';
+			//VIDE
 			context.fillText(this.pseudo, this.posX, this.posY - 10);
 		}
 	}
