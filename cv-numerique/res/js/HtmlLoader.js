2b19e88f5516148c80f6fd826dbbb79fd388d7de
Camille Fourmaintraux <camille.fourmaintraux.etu@univ-lille.fr>
2025-02-27 17:55:51 +0100

    Abandon pour le moment de créer une fonction HtmlLoader et éviter le duplicata de code. Voir ça plus tard

--- cv-numerique/res/js/HtmlLoader.js
+++ cv-numerique/res/js/HtmlLoader.js
@@ -1,13 +1,18 @@
- // Fonction pour charger un fichier HTML dans un élément donné
+ /*// Fonction pour charger un fichier HTML dans un élément donné
  function loadHTML(elementId, filename) {
     fetch(filename)
         .then(response => response.text())
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
+            console.log("Chargement réussie de "+filename);
         })
         .catch(error => console.error("Erreur de chargement : ", error));
 }
 
 // Charger le header et le footer
-loadHTML("header", "../parts/header.html");
-//loadHTML("footer", "footer.html");
+loadHTML("", "../parts/header.html");
+loadHTML("navigation", "../parts/navbar.html");
+loadHTML("footer", "../parts/footer.html");
+
+
+console.log("TEST HTMLLoader ");*/
