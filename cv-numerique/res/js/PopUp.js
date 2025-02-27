c5e29be9ee60b41158625445849f15cb8fb706d9
Camille Fourmaintraux <camille.fourmaintraux.etu@univ-lille.fr>
2025-02-27 16:31:52 +0100

    Minor fix popup resume

--- cv-numerique/res/js/PopUp.js
+++ cv-numerique/res/js/PopUp.js
@@ -1,7 +1,8 @@
 function displayResumeStage(){
-    alert("J’ai réalisé mon stage chez E-Mothep, une entreprise spécialisée dans les solutions logicielles pour la gestion des systèmes d’information.\n\
-        J’ai travaillé sur l’évolution d’AMAT, en intégrant de nouvelles fonctionnalités et en développant une API pour automatiser la mise à jour des référentiels, améliorant ainsi les processus internes.\n\
-        J’ai également rédigé de la rétro-documentation et contribué à une base de connaissances pour faciliter le travail futur.\n\
-        Ce stage m’a permis de développer des compétences en gestion de bases de données, développement d’API, documentation, et gestion de projet avec des outils comme DBeaver, Software AG Designer, Postman, et GitHub.\n\
+    alert("\
+        J’ai réalisé mon stage chez E-Mothep, une entreprise spécialisée dans les solutions logicielles pour la gestion des systèmes d’information.\n\n\
+        J’ai travaillé sur l’évolution d’AMAT, en intégrant de nouvelles fonctionnalités et en développant une API pour automatiser la mise à jour des référentiels, améliorant ainsi les processus internes.\n\n\
+        J’ai également rédigé de la rétro-documentation et contribué à une base de connaissances pour faciliter le travail futur.\n\n\
+        Ce stage m’a permis de développer des compétences en gestion de bases de données, développement d’API, documentation, et gestion de projet avec des outils comme DBeaver, Software AG Designer, Postman, et GitHub.\n\n\
         Cette expérience a renforcé mes compétences techniques et confirmé mon projet professionnel dans le développement logiciel.");
 }
