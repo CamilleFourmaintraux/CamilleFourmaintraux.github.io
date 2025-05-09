129c3ccef1fc72004a483f035e65ef2817815548
camillefourmaintrauxetu <camille.fourmaintraux.etu@univ-lille.fr>
2025-05-09 20:13:41 +0200

    added translations and a button back to menu to redirection page

--- src/pages/Redirection.tsx
+++ src/pages/Redirection.tsx
@@ -1,11 +1,17 @@
+import { NavLink } from "react-router";
+import { useTranslation } from "react-i18next";
+
 export default function RedirectionPage() {
+  const { t } = useTranslation();
   return (
     <>
       <div className="container">
         <h2>
-          <i className="fas fa-triangle-exclamation"></i> Redirection{" "}
+          <i className="fas fa-triangle-exclamation"></i>{" "}
+          {t("redirection.title")}{" "}
         </h2>
-        <p>Désolé, cette page n'existe pas.</p>
+        <p>{t("redirection.sorry")}</p>
+        <NavLink to="/"> {t("redirection.back")}</NavLink>
       </div>
     </>
   );
