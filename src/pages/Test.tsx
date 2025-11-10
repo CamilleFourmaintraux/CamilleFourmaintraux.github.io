9c14be469a8114836cb7efd138f0e6027c82be82
camillefourmaintrauxetu <camille.fourmaintraux.etu@univ-lille.fr>
2025-11-10 21:59:33 +0100

    Small changes

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -4,21 +4,12 @@ import { useTranslation } from "react-i18next";
 export default function RedirectionPage() {
   const { t } = useTranslation();
   return (
-    <>
-      <div className="container">
-        <h2>
-          <i className="fas fa-triangle-exclamation"></i>{" "}
-          {t("test.title")}
-        </h2>
-        <p>{t("test.main")}</p>
-        <NavLink to="/"> {t("test.back")}</NavLink>
-      </div>
-      <div className="container testFlexContainer">
-            <div className="testContainer">
-            </div>
-            <div className="testContainer">
-            </div>
-      </div>
-    </>
+    <div className="container">
+      <h2>
+        <i className="fas fa-triangle-exclamation"></i> {t("test.title")}
+      </h2>
+      <p>{t("test.main")}</p>
+      <NavLink to="/"> {t("test.back")}</NavLink>
+    </div>
   );
 }
